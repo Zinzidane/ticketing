0b306c934d33c008891f6bff5b79e22099e6229a
ivan <Ivan Muliarchyk>
2020-05-04 21:35:48 +0300

    Update Order mongoose model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@zzticketing/common';
+import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
   userId: string;
@@ -26,7 +27,9 @@ const orderSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    required: true
+    required: true,
+    enum: Object.values(OrderStatus),
+    default: OrderStatus.Created
   },
   expiresAt: {
     type: mongoose.Schema.Types.Date
