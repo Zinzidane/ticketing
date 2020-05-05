02dffc37e2b21e97a696d0378b8c8d13dee6aa30
ivan <Ivan Muliarchyk>
2020-05-05 13:58:11 +0300

    Implement cancelling user order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '../../models/order';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+it('cancel the user order', async () => {
+  const ticket = await buildTicket();
+  const user = global.getCookie();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  const { body: cancelledOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(cancelledOrder.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('returns and error if one user try to cancel not owned orders', async () => {
+  const ticket = await buildTicket();
+  const user = global.getCookie();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', global.getCookie())
+    .send()
+    .expect(401);
+});
