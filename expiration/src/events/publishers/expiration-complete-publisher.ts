7045bd6106e064acf9f75167459be65d2dffc018
ivan <Ivan Muliarchyk>
2020-05-24 16:58:21 +0300

    Publish expiration complete event on job processing

---
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, ExpirationCompleteEvent, Subjects } from "@zzticketing/common";
+
+export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+}
