66abc0f8882d942e7c0a430f02bb4f1237a23891
Ivan Muliarchyk <ya-gans@mail.ru>
2020-05-25 22:16:04 +0300

    Update test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@zzticketing/common";
 import { stripe } from "../../stripe";
+import { Payment } from "../../models/payment";
 
 // jest.mock("../../__mocks__/stripe.ts");
 
@@ -92,4 +93,11 @@ it("returns a 201 with valid inputs", async () => {
 
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual("usd");
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
 });
