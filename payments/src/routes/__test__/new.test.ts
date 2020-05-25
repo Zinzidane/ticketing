ed16170e5ce1333d929e0e9cc4701e73fcff23a6
Ivan Muliarchyk <ya-gans@mail.ru>
2020-05-25 22:15:07 +0300

    Create payment in route

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -91,4 +91,5 @@ it("returns a 201 with valid inputs", async () => {
   });
 
   expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual("usd");
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from "@zzticketing/common";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -36,11 +37,17 @@ router.post(
     }
 
     // Stripe works with minimal value. So for the usd it is a cent
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: "usd",
       amount: order.price * 100,
       source: token,
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
