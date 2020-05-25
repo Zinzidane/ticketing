9855ce94bdcffd30ea641bd43f4e141e27bafe9e
Ivan Muliarchyk <ya-gans@mail.ru>
2020-05-25 21:44:42 +0300

    Mock stipe client

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,42 +1,45 @@
-import request from 'supertest';
-import mongoose from 'mongoose';
-import { app } from '../../app';
-import { Order } from '../../models/order';
-import { OrderStatus } from '@zzticketing/common';
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@zzticketing/common";
+import { stripe } from "../../stripe";
 
-it('returns a 404 if purchasing an order that does not exist', async () => {
+jest.mock("../../__mocks__/stripe.ts");
+
+it("returns a 404 if purchasing an order that does not exist", async () => {
   await request(app)
-    .post('/api/payments')
-    .set('Cookie', global.getCookie())
+    .post("/api/payments")
+    .set("Cookie", global.getCookie())
     .send({
-      token: 'sasdasd',
-      orderId: mongoose.Types.ObjectId().toHexString()
+      token: "sasdasd",
+      orderId: mongoose.Types.ObjectId().toHexString(),
     })
     .expect(404);
 });
 
-it('returns a 401 if purchasing an order that does belong to the user', async () => {
+it("returns a 401 if purchasing an order that does belong to the user", async () => {
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId: mongoose.Types.ObjectId().toHexString(),
     version: 0,
     price: 20,
-    status: OrderStatus.Created
+    status: OrderStatus.Created,
   });
 
   await order.save();
 
   await request(app)
-    .post('/api/payments')
-    .set('Cookie', global.getCookie())
+    .post("/api/payments")
+    .set("Cookie", global.getCookie())
     .send({
-      token: 'sasdasd',
-      orderId: order.id
+      token: "sasdasd",
+      orderId: order.id,
     })
     .expect(401);
 });
 
-it('returns a 400 if purchasing a cancelled order', async () => {
+it("returns a 400 if purchasing a cancelled order", async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
 
   const order = Order.build({
@@ -44,19 +47,45 @@ it('returns a 400 if purchasing a cancelled order', async () => {
     userId,
     version: 0,
     price: 20,
-    status: OrderStatus.Cancelled
+    status: OrderStatus.Cancelled,
   });
 
   await order.save();
 
   await request(app)
-    .post('/api/payments')
-    .set('Cookie', global.getCookie(userId))
+    .post("/api/payments")
+    .set("Cookie", global.getCookie(userId))
     .send({
-      token: 'sasdasd',
-      orderId: order.id
+      token: "sasdasd",
+      orderId: order.id,
     })
-    .expect(200);
+    .expect(400);
+});
 
-    
+it("returns a 201 with valid inputs", async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.getCookie(userId))
+    .send({
+      token: "tok_visa",
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual("tok_visa");
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual("usd");
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,21 +1,22 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-import { requireAuth, validateRequest, NotFoundError, NotAuthorizedError, OrderStatus, BadRequestError } from '@zzticketing/common';
-import { Order } from '../models/order';
-import { stripe } from '../stripe';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
+  BadRequestError,
+} from "@zzticketing/common";
+import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
-router.post('/api/payments',
+router.post(
+  "/api/payments",
   requireAuth,
-  [
-    body('token')
-      .not()
-      .isEmpty(),
-    body('orderId')
-    .not()
-    .isEmpty()
-  ],
+  [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
@@ -31,17 +32,18 @@ router.post('/api/payments',
     }
 
     if (order.status === OrderStatus.Cancelled) {
-      throw new BadRequestError('Cannot pay for an cancelled order');
+      throw new BadRequestError("Cannot pay for an cancelled order");
     }
 
     // Stripe works with minimal value. So for the usd it is a cent
     await stripe.charges.create({
-      currency: 'usd',
+      currency: "usd",
       amount: order.price * 100,
-      source: token
+      source: token,
     });
 
-    res.send({ success: true });
-});
+    res.status(201).send({ success: true });
+  }
+);
 
 export { router as createChargeRouter };
